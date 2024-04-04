e53f1d7bffd373d9f6c9a7e3eae75f1979e469bd
JeongMinkyo <jeong.minkyo@hyundai-autoever.com>
2024-04-04 13:26:01 +0900

    refactor(key-pair): jailgas key pair 수정
    - key 생성으로 변경

--- key-pair/main.tf
+++ key-pair/main.tf
@@ -1,12 +1,23 @@
-# aws_key_pair resource 
+# Generates a secure private key and encodes it as PEM
+resource "tls_private_key" "key_pair" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Create the Key Pair
 resource "aws_key_pair" "jailgas-ssh-key" {
   key_name   = var.name
-
-  public_key = file("../../../../ssh/${var.name}-ssh-key.pub")
+  public_key = tls_private_key.key_pair.public_key_openssh
   
   tags = {
     Name             = "${var.name}-ssh-key"
     Environment      = var.env
     TerraformManaged = "true"
   }
+}
+
+# Save Pem Key
+resource "local_file" "ssh_key" {
+  filename = "${path.module}/${aws_key_pair.jailgas-ssh-key.key_name}.pem"
+  content  = tls_private_key.key_pair.private_key_pem
 }
